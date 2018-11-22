028c41eeeb5e7936a6c18357eb5e4e7607454df5
Jinjun Huang <jinjhuang@thoughtworks.com>
2018-11-22 17:56:05 +0800

    first commit for lunch session

---
+++ src/main/java/com/tw/dps/hibernatedemo3/Hibernatedemo3Application.java
@@ -0,0 +1,12 @@
+package com.tw.dps.hibernatedemo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hibernatedemo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hibernatedemo3Application.class, args);
+    }
+}
---
+++ src/test/java/com/tw/dps/hibernatedemo3/Hibernatedemo3ApplicationTests.java
@@ -0,0 +1,39 @@
+package com.tw.dps.hibernatedemo3;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Hibernatedemo3ApplicationTests {
+    private static final SessionFactory ourSessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            ourSessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+    @Test
+    public void contextLoads() {
+        final Query query = getSession().createQuery("from User where userId = " + 1);
+        User user = (User) query.getSingleResult();
+        System.out.println(">>>>>>>>>>"+user.getUserAssignmentsByUserId().size());
+    }
+
+}
